ae6619a9c44751a2ba570d98a6cb8b4007ddd3f5
Glitch (tmonks-messageboard) <none>
2020-01-23 20:20:07 +0000

    Functional tests for /api/threads/:board

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -19,6 +19,7 @@ suite('Functional Tests', function() {
     
     let thread_id;
     const delete_password = 'Password123';
+    let thread_count;
     
     suite('POST', function() {
       test('POST new thread', done => {
@@ -48,10 +49,41 @@ suite('Functional Tests', function() {
             assert.notProperty(res.body[0], 'delete_password', 'delete_password property is hidden');
             assert.notProperty(res.body[0], 'reported', 'reported property is hidden');
             thread_id = res.body[0]._id;
+            thread_count = res.body.length;
+            console.log(thread_count + " threads found. Working with " + thread_id);
             done();
           });
       })
     });
+    
+    suite('PUT', function() {
+      
+      test('PUT to report a thread', done => {
+        chai.request(server)
+          .put('/api/threads/testing')
+          .send({ thread_id })
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, 'success');
+            done();
+          });
+      });  
+      
+      // Realized this won't work since the 'reported' attribute is hidden
+//       test('Reported thread is now marked reported', done => {
+//         chai.request(server)
+//           .get('/api/threads/testing')
+//           .end((err, res) => {
+//             assert.equal(res.status, 200);
+//             assert.atLeast(res.body.length, 1, 'At least one thread was returned');
+//             let indexOfThread = res.body.indexOf(x => x._id === thread_id);
+//             assert.atLeast(indexOfThread, 0, 'Our thread is present');
+//             assert.property(res.body[indexOfThread], 'reported' , 'Thread has the reported')
+//           });
+        
+//       });
+      
+    }); // PUT suite
     
     suite('DELETE', function() {
       test('DELETE thread with wrong password', done => {
@@ -71,15 +103,29 @@ suite('Functional Tests', function() {
           .send({ thread_id, delete_password })
           .end((err, res) => {
             assert.equal(res.status, 200);
-            assert.equal(res.text, 'success');
+            assert.equal(res.text, 'success', "Response is 'success'");
             done();
           });
       });
-    });
-    
-    suite('PUT', function() {
       
-    });
+      test('Deleted thread is gone', done => {
+        chai.request(server)
+          .get('/api/threads/testing')
+          .end((err, res) => {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.length, thread_count - 1, 'There is now one less thread');
+            if (res.body.length > 0) {
+              // find the index of the thread with thread_id if it exists
+              const indexOfThread = res.body.indexOf(x => x._id === thread_id);
+              assert.equal(indexOfThread, -1, 'thread_id is not found in the returned array');
+            }
+            done();
+          });
+      });
+      
+    }); // DELETE suite
+    
+
     
 
   });
